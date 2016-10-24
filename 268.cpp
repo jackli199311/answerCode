c333a2ae7ec3c137d5fc98218486196606f709c6
jackli199311 <jackli199311@gmail.com>
2016-10-24 15:02:18 -0500

    Create 268.cpp

---
+++ 268.cpp
@@ -0,0 +1,11 @@
+//Missing Number
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int len=nums.size();
+        int sum=0;
+        for(int i=0;i<len;++i)
+            sum+=nums[i];
+        return len*(len+1)/2-sum;
+    }
+};
