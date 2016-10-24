e699fa92d203cbab760fe5e7b12e834b7d53af02
jackli199311 <jackli199311@gmail.com>
2016-10-24 14:54:00 -0500

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int> &A, int target) {
+        // write your code here
+        int n = A.size();
+        int low = 0, high = n - 1;
+        while (low < high) {
+            int mid = (high - low)/2 + low;
+            if (A[mid] > A[high]) {
+                low = mid + 1;
+            }
+            else 
+                high = mid;
+        }
+        
+        int piv = low;
+        low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (high - low)/2 + low;
+            int trueMid = (piv + mid)%n;
+            if (A[trueMid] == target) {
+                return trueMid;
+            } else if (A[trueMid] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid -1;
+            }
+        }
+        
+        return -1;
+    }
+    
+};
